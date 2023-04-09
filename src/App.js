6ca7f5ad8fbafa3a8ced01e6aaa0a68ffd6fa1c0
Sergio Ortiz <sergioortiz170992@gmail.com>
2023-04-09 20:20:01 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sergioortizmobile.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portafolio Sergio
         </a>
       </header>
     </div>
